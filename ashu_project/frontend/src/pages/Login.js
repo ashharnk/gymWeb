289347e85d886f19dbdf9944d2dcfc776418c2bf
ashharnk <46646600+ashharnk@users.noreply.github.com>
2025-04-06 19:44:11 +0530

    Add files via upload

---
+++ ashu_project/frontend/src/pages/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('https://fitness-app-dr9e.onrender.com/api/auth/login', { email, password });
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('name', res.data.name);
+      localStorage.setItem('role', res.data.role);
+      window.location.href = '/';
+    } catch (err) {
+      alert('Invalid Creds')
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="max-w-md w-full bg-gray-800 p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-extrabold text-center text-red-500 mb-6">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label className="block text-white text-lg mb-2">Email:</label>
+            <input 
+              type="email" 
+              className="w-full p-3 rounded border border-gray-700 focus:outline-none focus:border-red-500"
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block text-white text-lg mb-2">Password:</label>
+            <input 
+              type="password" 
+              className="w-full p-3 rounded border border-gray-700 focus:outline-none focus:border-red-500"
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required
+            />
+          </div>
+          <button type="submit" className="w-full bg-red-500 hover:bg-red-600 text-white py-3 rounded font-bold transition duration-200">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
